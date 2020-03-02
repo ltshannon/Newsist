fc50f52a08c3a9870540f127c97d526de652952a
Larry Shannon <larry@breakawaydesign.com>
2020-03-01 18:10:51 -0800

    Initial Commit

---
+++ Newsist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Newsist
+//
+//  Created by Larry Shannon on 3/1/20.
+//  Copyright © 2020 Larry Shannon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
