56273f1be098f06259a1e9dd716f45e6dd27d4a1
Larry Shannon <larry@breakawaydesign.com>
2020-04-02 15:17:54 -0700

    Commit for build 12

--- Newsist/BNOView.swift
+++ Newsist/BNOView.swift
@@ -60,9 +60,9 @@ struct Home : View {
             
             ZStack{
                 
-                World(model: covidData, show: self.$show).opacity(self.index == 0 ? 1.0 : 0.0)
+                DisplayData(type: "World", model: covidData).opacity(self.index == 0 ? 1.0 : 0.0)
                 
-                usaCites(model: covidData).opacity(self.index == 1 ? 1.0 : 0.0)
+                DisplayData(type: "USA", model: covidData).opacity(self.index == 1 ? 1.0 : 0.0)
                 
                 Calls().opacity(self.index == 2 ? 1.0 : 0.0)
             }
@@ -129,72 +129,133 @@ struct appBar : View {
     }
 }
 
-struct World: View {
+struct DisplayData: View {
+    let type: String
     @ObservedObject var model: BNOViewModel
-    @Binding var show : Bool
+    @Environment(\.verticalSizeClass) var verticalSizeClass: UserInterfaceSizeClass?
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass: UserInterfaceSizeClass?
+    @State var index = 1
     
     var body: some View {
         VStack(alignment: .leading) {
             HStack {
-                Text("Country")
-                    .padding()
-                    .frame(width: 145, alignment: .leading)
-                Text("Confirmed")
-                    .frame(width: 100, alignment: .leading)
-                Text("Deaths")
-                    .frame(width: 80, alignment: .leading)
-                Text("New")
-                    .frame(width: 80, alignment: .leading)
-            }
-            .frame(height: 40)
-
-            List (model.countryDataSet, id: \.self) { values in
-                NavigationLink(destination: BNODetailView(url: values.href)) {
+                Button(action: {
+                    self.model.sort(sortBy: .country)
+                    self.index = 0
+                }) {
                     HStack {
-                        Text(values.country)
-                            .frame(width: 125, alignment: .leading)
-                        Text("\(values.confirmed)")
+                        Text("Country")
+                            .padding()
+                            .frame(width: 95, height: 25, alignment: .leading)
+//                            .background(Color.pink)
+                        if index == 0 {
+                            Image(systemName: model.countryArrow ? "arrow.up" : "arrow.down")
+                                .resizable()
+                                .frame(width: 15, height: 15, alignment: .leading)
+                                .foregroundColor(.black)
+                        }
+                    }
+                    .frame(width: 130)
+                }
+                Button(action: {
+                    self.model.sort(sortBy: .confirmed)
+                    self.index = 1
+                }) {
+                    HStack {
+                        Text("Confirmed")
+                            .frame(width: 90, alignment: .leading)
+//                            .background(Color.pink)
+                        if index == 1 {
+                            Image(systemName: model.confirmedArrow ? "arrow.up" : "arrow.down")
+                                .resizable()
+                                .frame(width: 15, height: 15, alignment: .leading)
+                                .foregroundColor(.black)
+                        }
+                    }
+                    .frame(width: 120)
+
+                }
+                Button(action: {
+                    self.model.sort(sortBy: .deaths)
+                    self.index = 2
+                }) {
+                    HStack {
+                        Text("Deaths")
+                            .frame(width: 60, alignment: .leading)
+//                            .background(Color.pink)
+                        if index == 2 {
+                            Image(systemName: model.deathsArrow ? "arrow.up" : "arrow.down")
+                                .resizable()
+                                .frame(width: 15, height: 15, alignment: .leading)
+                                .foregroundColor(.black)
+                            }
+                        }
+                    .frame(width: 80)
+                }
+                Button(action: {
+                    self.model.sort(sortBy: .newDeaths)
+                    self.index = 3
+                }) {
+                    HStack {
+                        Text("New")
+                            .frame(width: 40, alignment: .leading)
+//                            .background(Color.pink)
+                        if index == 3 {
+                            Image(systemName: model.newDeathsArrow ? "arrow.up" : "arrow.down")
+                                .resizable()
+                                .frame(width: 15, height: 15, alignment: .leading)
+                                .foregroundColor(.black)
+                        }
+                    }
+                    .frame(width: 80)
+                }
+                if horizontalSizeClass == .regular && verticalSizeClass == .compact || horizontalSizeClass == .regular && verticalSizeClass == .regular  {
+                    Button(action: {
+                        self.model.sort(sortBy: .deathRate)
+                    }) {
+                        Text("Death Rate")
+                    }
                             .frame(width: 100, alignment: .leading)
-                        Text("\(values.deaths)")
-                            .frame(width: 80, alignment: .leading)
-                        Text("\(values.newDeaths)")
+                    Button(action: {
+                        self.model.sort(sortBy: .seriousCritical)
+                    }) {
+                        Text("Critical")
+                            .frame(width: 100, alignment: .leading)
+                    }
+                    Button(action: {
+                        self.model.sort(sortBy: .recovered)
+                    }) {
+                        Text("Recovered")
                             .frame(width: 100, alignment: .leading)
                     }
                 }
             }
-        }
-    }
-}
-
-struct usaCites : View {
-    @ObservedObject var model: BNOViewModel
- 
-    var body: some View {
-        VStack {
-            HStack {
-                Text("City")
-                    .padding()
-                    .frame(width: 145, alignment: .leading)
-                Text("Confirmed")
-                    .frame(width: 100, alignment: .leading)
-                Text("Deaths")
-                    .frame(width: 80, alignment: .leading)
-                Text("New")
-                    .frame(width: 80, alignment: .leading)
-            }
             .frame(height: 40)
+
             
-            List(model.usaDataSet, id: \.self) { values in
+            List (type == "World" ? model.countryDataSet : model.usaDataSet, id: \.self) { values in
                 NavigationLink(destination: BNODetailView(url: values.href)) {
                     HStack {
                         Text(values.country)
-                            .frame(width: 125, alignment: .leading)
+                            .frame(width: self.index == 0 ? 130 : 130, alignment: .leading)
                         Text("\(values.confirmed)")
-                            .frame(width: 100, alignment: .leading)
+                            .frame(width: self.index == 1 ? 110 : 110, alignment: .leading)
                         Text("\(values.deaths)")
-                            .frame(width: 80, alignment: .leading)
-                        Text("\(values.newDeaths)")
-                            .frame(width: 100, alignment: .leading)
+                            .frame(width: self.index == 2 ? 90 : 80, alignment: .leading)
+                        if self.horizontalSizeClass == .compact && self.verticalSizeClass == .regular {
+                            Text("\(values.newDeaths)")
+                                .frame(width: 100, alignment: .leading)
+                        }
+                        else {
+                            Text("\(values.newDeaths)")
+                                .frame(width: 80, alignment: .leading)
+                            Text("\(String(format: "%.2f",values.deathRate))%")
+                                .frame(width: 100, alignment: .leading)
+                            Text("\(values.seriousCritical)")
+                                .frame(width: 100, alignment: .leading)
+                            Text("\(values.recovered)")
+                                .frame(width: 100, alignment: .leading)
+                        }
                     }
                 }
             }
--- Newsist/BNOViewModel.swift
+++ Newsist/BNOViewModel.swift
@@ -9,20 +9,31 @@
 import SwiftUI
 import SwiftSoup
 
+enum SortType {
+    case country
+    case confirmed
+    case newCases
+    case deaths
+    case newDeaths
+    case deathRate
+    case seriousCritical
+    case recovered
+}
+
 struct BNOData: Decodable, Hashable {
     let country: String
     let confirmed: Int
-    let newCases: String
-    let deaths: String
-    let newDeaths: String
-    let deathRate: String
-    let seriousCritical: String
-    let recovered: String
+    let newCases: Int
+    let deaths: Int
+    let newDeaths: Int
+    let deathRate: Double
+    let seriousCritical: Int
+    let recovered: Int
     let yesterdayConfirmed: String
     let yesterdayDeaths: String
     let href: String
     
-    init(country: String, confirmed: Int, newCases: String, deaths: String, newDeaths: String, deathRate: String, seriousCritical: String, recovered: String, yesterdayConfirmed: String, yesterdayDeaths: String, href: String) {
+    init(country: String, confirmed: Int, newCases: Int, deaths: Int, newDeaths: Int, deathRate: Double, seriousCritical: Int, recovered: Int, yesterdayConfirmed: String, yesterdayDeaths: String, href: String) {
         self.country = country
         self.confirmed = confirmed
         self.newCases = newCases
@@ -38,8 +49,14 @@ struct BNOData: Decodable, Hashable {
 }
 
 class BNOViewModel: ObservableObject {
-@Published var countryDataSet: [BNOData] = []
-@Published var usaDataSet: [BNOData] = []
+    @Published var countryDataSet: [BNOData] = []
+    @Published var usaDataSet: [BNOData] = []
+    @Published var countryArrow: Bool = true
+    @Published var confirmedArrow: Bool = true
+    @Published var newCasesArrow: Bool = true
+    @Published var newDeathsArrow: Bool = true
+    @Published var deathsArrow: Bool = true
+
 
     var countryArray: [BNOData] = []
     var usaArray: [BNOData] = []
@@ -110,6 +127,72 @@ class BNOViewModel: ObservableObject {
         
     }
     
+    func sort(sortBy: SortType) {
+        
+        switch sortBy {
+        case .country:
+            if countryArrow {
+                self.countryArray.sort{$0.country < $1.country}
+                self.usaArray.sort{$0.country < $1.country}
+            } else {
+                self.countryArray.sort{$0.country > $1.country}
+                self.usaArray.sort{$0.country > $1.country}
+            }
+            countryArrow.toggle()
+        case .confirmed:
+            if confirmedArrow {
+                self.countryArray.sort{$0.confirmed > $1.confirmed}
+                self.usaArray.sort{$0.confirmed > $1.confirmed}
+            } else {
+                self.countryArray.sort{$0.confirmed < $1.confirmed}
+                self.usaArray.sort{$0.confirmed < $1.confirmed}
+            }
+            confirmedArrow.toggle()
+        case .newCases:
+            if newCasesArrow {
+                self.countryArray.sort{$0.newCases > $1.newCases}
+                self.usaArray.sort{$0.newCases > $1.newCases}
+            }
+            else {
+                self.countryArray.sort{$0.newCases < $1.newCases}
+                self.usaArray.sort{$0.newCases < $1.newCases}
+            }
+            newCasesArrow.toggle()
+        case .deaths:
+            if deathsArrow {
+                self.countryArray.sort{$0.deaths > $1.deaths}
+                self.usaArray.sort{$0.deaths > $1.deaths}
+            } else {
+                self.countryArray.sort{$0.deaths < $1.deaths}
+                self.usaArray.sort{$0.deaths < $1.deaths}
+            }
+            deathsArrow.toggle()
+        case .newDeaths:
+            if newDeathsArrow {
+                self.countryArray.sort{$0.newDeaths > $1.newDeaths}
+                self.usaArray.sort{$0.newDeaths > $1.newDeaths}
+            } else {
+                self.countryArray.sort{$0.newDeaths < $1.newDeaths}
+                self.usaArray.sort{$0.newDeaths < $1.newDeaths}
+            }
+            newDeathsArrow.toggle()
+        case .deathRate:
+            self.countryArray.sort{$0.deathRate > $1.deathRate}
+            self.usaArray.sort{$0.deathRate > $1.deathRate}
+        case .seriousCritical:
+            self.countryArray.sort{$0.seriousCritical > $1.seriousCritical}
+            self.usaArray.sort{$0.seriousCritical > $1.seriousCritical}
+        case .recovered:
+            self.countryArray.sort{$0.recovered > $1.recovered}
+            self.usaArray.sort{$0.recovered > $1.recovered}
+        }
+        
+        DispatchQueue.main.async {
+            self.countryDataSet = self.countryArray
+            self.usaDataSet = self.usaArray
+        }
+    }
+    
     func parseTable(bodyContent: Element, table: String) {
         
         do {
@@ -149,12 +232,12 @@ class BNOViewModel: ObservableObject {
         
         var country = ""
         var cases = 0
-        var newCases = ""
-        var deaths = ""
-        var newDeaths = ""
-        var deathRate = ""
-        var seriousCritical = ""
-        var recovered = ""
+        var newCases = 0
+        var deaths = 0
+        var newDeaths = 0
+        var deathRate = 0.0
+        var seriousCritical = 0
+        var recovered = 0
         var yesterdayConfirmed = ""
         var yesterdayDeaths = ""
         
@@ -162,24 +245,25 @@ class BNOViewModel: ObservableObject {
         do {
             for str in tdArray {
                 let text = try str.text()
+                var item = text.replacingOccurrences(of: ",", with: "")
+                item = item.replacingOccurrences(of: "%", with: "")
                 switch index {
                 case 0:
                     country = text
                 case 1:
-                    let item = text.replacingOccurrences(of: ",", with: "")
                     cases = Int(item) != nil ? Int(item)! : 0
                 case 2:
-                    newCases = text
+                    newCases = Int(item) != nil ? Int(item)! : 0
                 case 3:
-                    deaths = text
+                    deaths = Int(item) != nil ? Int(item)! : 0
                 case 4:
-                    newDeaths = text
+                    newDeaths = Int(item) != nil ? Int(item)! : 0
                 case 5:
-                    deathRate = text
+                    deathRate = Double(item) != nil ? Double(item)! : 0
                 case 6:
-                    seriousCritical = text
+                    seriousCritical = Int(item) != nil ? Int(item)! : 0
                 case 7:
-                    recovered = text
+                    recovered = Int(item) != nil ? Int(item)! : 0
                 case 8:
                     yesterdayConfirmed = text
                 case 9:
